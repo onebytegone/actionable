3a2f9e75443c369e0300cc756ea1811d36f44041
Ethan Smith <ethan@onebytegone.com>
2015-05-18 19:18:45 -0400

    Added base function definitions

--- Actionable/Actionable.swift
+++ Actionable/Actionable.swift
@@ -12,4 +12,16 @@ class Actionable {
    func printHello() {
       println("Hello")
    }
+
+   func on(event: String, handler: () -> Void) {
+      fatalError("on(event:, handler:) has not been created yet")
+   }
+
+   func off(event: String, handler: () -> Void = { _ in }) {
+      fatalError("off(event:, handler:) has not been created yet")
+   }
+
+   func trigger(event: String) {
+      fatalError("trigger(event:) has not been created yet")
+   }
 }
