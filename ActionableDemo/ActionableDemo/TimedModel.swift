b881a91f80e52e9f5e0a72c49d2c137d5b988f99
Ethan Smith <ethan@onebytegone.com>
2015-05-18 22:07:46 -0400

    Added simple demo

---
+++ ActionableDemo/ActionableDemo/TimedModel.swift
@@ -0,0 +1,23 @@
+//
+//  TimedModel.swift
+//  ActionableDemo
+//
+//  Created by Ethan Smith on 5/18/15.
+//  Copyright (c) 2015 Ethan Smith. All rights reserved.
+//
+
+import Foundation
+import Actionable
+
+class TimedModel : NSObject {
+   var actionable = Actionable();
+
+   override init() {
+      super.init()
+      NSTimer.scheduledTimerWithTimeInterval(0.05, target: self, selector: "timerFired", userInfo: nil, repeats: true)
+   }
+
+   func timerFired() {
+      actionable.trigger("changed:timer")
+   }
+}
---
+++ ActionableDemo/ActionableDemo/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  ActionableDemo
+//
+//  Created by Ethan Smith on 5/18/15.
+//  Copyright (c) 2015 Ethan Smith. All rights reserved.
+//
+
+import UIKit
+import Actionable
+
+class ViewController: UIViewController {
+   var model = TimedModel()
+   let label = UILabel()
+   let toggleButton = UIButton()
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+
+      toggleButton.setTitle("Turn on", forState: UIControlState.Normal)
+      toggleButton.setTitle("Turn off", forState: UIControlState.Selected)
+      toggleButton.backgroundColor = UIColor.lightGrayColor()
+      toggleButton.addTarget(self, action: "toggleState:", forControlEvents: UIControlEvents.TouchUpInside)
+      self.view.addSubview(toggleButton)
+
+      label.text = "Press the button..."
+      self.view.addSubview(label)
+   }
+
+   func toggleState(sender: UIButton) {
+      if sender.selected {
+         model.actionable.allOff("changed:timer")
+      } else {
+         model.actionable.on("changed:timer", handler: {
+            self.label.text = "Date is \(NSDate.timeIntervalSinceReferenceDate())"
+         });
+      }
+
+      sender.selected = !sender.selected;
+   }
+
+   override func viewDidLayoutSubviews() {
+      label.frame = CGRectMake(30, 50, CGRectGetWidth(self.view.bounds)-60, 20)
+      toggleButton.frame = CGRectMake(30, CGRectGetMaxY(label.frame)+20, CGRectGetWidth(self.view.bounds)-60, 50)
+   }
+
+}
