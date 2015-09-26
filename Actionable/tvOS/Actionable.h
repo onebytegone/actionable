f3f33a2b22276b24cc53570ea4d1faa4a875c231
Ethan Smith <ethan@onebytegone.com>
2015-09-26 09:34:48 -0400

    Added tvOS target

---
+++ Actionable/tvOS/Actionable.h
@@ -0,0 +1,19 @@
+//
+//  Actionable.h
+//  Actionable
+//
+//  Created by Ethan Smith on 9/26/15.
+//  Copyright (c) 2015 Ethan Smith. All rights reserved.
+//
+
+@import Foundation;
+
+//! Project version number for Actionable.
+FOUNDATION_EXPORT double ActionableVersionNumber;
+
+//! Project version string for Actionable.
+FOUNDATION_EXPORT const unsigned char ActionableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Actionable/PublicHeader.h>
+
+
